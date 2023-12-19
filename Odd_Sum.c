ef0eff44ee278aa6269965adca35f80e93fd6180
Manisanthanreddy Kanamathareddy <manisanthanreddy55@gmail.com>
2023-12-19 10:25:25 +0530

    #include<stdio.h> int main() { 	int i,a; 	scanf("%d",&a); 	int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int odd_sum=0; 	for(i=0;i<a;i++) 	{ 	  if(arr[i]%2==1) 	  { 	  	odd_sum=odd_sum+arr[i]; 	  } 	} 	printf("%d",odd_sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,a;
+	scanf("%d",&a);
+	int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int odd_sum=0;
+	for(i=0;i<a;i++)
+	{
+	  if(arr[i]%2==1)
+	  {
+	  	odd_sum=odd_sum+arr[i];
+	  }
+	}
+	printf("%d",odd_sum);
+}
