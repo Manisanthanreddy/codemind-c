df45188540338d1ab949d7d6ba33c70ad21a381e
Manisanthanreddy Kanamathareddy <manisanthanreddy55@gmail.com>
2023-12-19 10:42:50 +0530

    #include<stdio.h> int main() { 	int i,a; 	scanf("%d",&a); 	int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int even_sum=0; 	for(i=0;i<a;i++) 	{ 	  if(i%2==0) 	  { 	  	even_sum=even_sum+arr[i]; 	  } 	} 	printf("%d",even_sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,a;
+	scanf("%d",&a);
+	int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int even_sum=0;
+	for(i=0;i<a;i++)
+	{
+	  if(i%2==0)
+	  {
+	  	even_sum=even_sum+arr[i];
+	  }
+	}
+	printf("%d",even_sum);
+}
